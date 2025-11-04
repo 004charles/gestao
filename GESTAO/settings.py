9f0f05c94971400538e320571b852cd8be7cb728
004charles <muquissicarlos@gmail.com>
2025-11-04 14:46:46 +0100

    deploy

--- GESTAO/settings.py
+++ GESTAO/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-(mg3=za^e#j4cg1y1z1xaop#vz*&tbsq^wvn7+w*5s#_dp_tti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'gestao-dxyi.onrender.com',
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com'  
+]
 
 
 # Application definition
@@ -112,10 +117,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
